a4ef3addf61fe8c0c48ce7cf90725a3b368b1075
Pham Tuan <tuan.pv@ptf.com.vn>
2024-02-01 10:12:06 +0700

    init project

---
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -0,0 +1,13 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hoidanit/laptopshop/LaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hoidanit.laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
