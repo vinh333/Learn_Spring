818c3639d4b5f17703ce55f6829f7683f34eb071
Vinh3 <101193855+vinh333@users.noreply.github.com>
2024-09-23 15:50:08 +0700

    update and fix dependence

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Greetings d";
+        return "Greetings dit me";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin access this page";
     }
 }
