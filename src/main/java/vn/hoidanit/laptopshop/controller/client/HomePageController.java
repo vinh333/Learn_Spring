5b8b36c565b596f250c8d9da52fb8c05356f3e5b
Vinh3 <101193855+vinh333@users.noreply.github.com>
2024-11-09 10:58:10 +0700

    Mapper Class

--- src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/HomePageController.java
@@ -2,27 +2,66 @@ package vn.hoidanit.laptopshop.controller.client;
 
 import java.util.List;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
 import vn.hoidanit.laptopshop.service.ProductService;
+import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class HomePageController {
-    private final ProductService productService;
 
-    public HomePageController(ProductService productService) {
+    private final ProductService productService;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public HomePageController(
+            ProductService productService,
+            UserService userService,
+            PasswordEncoder passwordEncoder) {
         this.productService = productService;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/")
     public String getHomePage(Model model) {
         List<Product> products = this.productService.getAllProducts();
         model.addAttribute("products", products);
-
         return "client/homepage/show";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO) {
+        User user = this.userService.registerDTOtoUser(registerDTO);
+
+        String hashPassword = this.passwordEncoder.encode(user.getPassword());
+
+        user.setPassword(hashPassword);
+        user.setRole(this.userService.getRoleByName("USER"));
+        // save
+        this.userService.handleSaveUser(user);
+        return "redirect:/login";
+
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+
+        return "client/auth/login";
     }
 
 }
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import vn.hoidanit.laptopshop.domain.Role;
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
 import vn.hoidanit.laptopshop.repository.RoleRepository;
 import vn.hoidanit.laptopshop.repository.UserRepository;
 
@@ -46,4 +47,11 @@ public class UserService {
         return this.roleRepository.findByName(name);
     }
 
+    public User registerDTOtoUser(RegisterDTO registerDTO) {
+        User user = new User();
+        user.setFullName(registerDTO.getFirstName() + " " + registerDTO.getLastName());
+        user.setEmail(registerDTO.getEmail());
+        user.setPassword(registerDTO.getPassword());
+        return user;
+    }
 }
